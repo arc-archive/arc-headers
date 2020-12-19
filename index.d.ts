548f84c38ea7f218adf2e5cbea3d24f04a963697
Pawel Psztyc <jarrodek@gmail.com>
2020-12-19 14:55:25 -0800

    feat: moving ArcHeaders here from electron request
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 export { HeadersParser } from './src/HeadersParser';
 export { HeadersEditorElement } from './src/HeadersEditorElement';
 export { HeadersListElement } from './src/HeadersListElement';
+export { ArcHeaders } from './src/ArcHeaders';
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 export { HeadersParser } from './src/HeadersParser.js';
 export { HeadersEditorElement } from './src/HeadersEditorElement.js';
 export { HeadersListElement } from './src/HeadersListElement.js';
-
+export { ArcHeaders } from './src/ArcHeaders.js';
